d1b65094fdf6b868fd081efdd96f0e36cff77c69
Eugene <snegur.1998@gmail.com>
2022-06-30 20:36:54 +0300

    Add js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-export const hotel = new Object();
+export const hotel = {
+  quantityOfPlaces: 30,
+  priceByPlace: 20,
+  bankAccount: 0,
+  guests: {},
+
+  getLength() {
+    return Object.keys(this.guests).length;
+  },
+
+  getActualFreePlace() {
+    return this.quantityOfPlaces - this.getLength();
+  },
+
+  paidPerPlace() {
+    this.bankAccount += this.priceByPlace;
+  },
+
+  checkInGuest(firstName, lastName, money) {
+    if (this.getActualFreePlace() === 0) {
+      return "Sorry, we haven't free spaces";
+    } else if (money < this.priceByPlace) {
+      return "Sorry, you haven't enough money";
+    } else {
+      this.guests[this.getLength()] = {
+        firstName,
+        lastName,
+        money: money - this.priceByPlace,
+      };
+      this.paidPerPlace();
+    }
+  },
+};
